d17b693f6c62ae56f4b232096abec1a1b1135b8a
Aksel Villela <jonhaksel@gmail.com>
2024-11-15 23:47:27 -0600

    se hizo la vista contacto y catalogo

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,24 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/login', function(req, res, next) {
+
   res.render('login', { title: ' Login | Odin' });
 })
-
-router.get('/catalogo', function(req, res, next) {
-  res.render('catalogo', { title:' Catálogo | Odin' });
-})
-
 
 router.get('/contacto', function(req, res, next) {
   res.render('contacto', { title: ' Contacto | Odin' });
 })
+
+
+router.get('/catalogo', (req, res) => {
+  const productos = [
+    { nombre: 'Producto 1', descripcion: 'Descripción del producto 1', precio: 100, imagen: '../images/incinerator.png' },
+    { nombre: 'Producto 2', descripcion: 'Descripción del producto 2', precio: 200, imagen: '../images/iron_man.png' },
+    { nombre: 'Producto 3', descripcion: 'Descripción del producto 3', precio: 300, imagen: '../images/minecraft.jpg' },
+    { nombre: 'Producto 4', descripcion: 'Descripción del producto 4', precio: 400, imagen: '../images/thor_pc.png' },
+  ];
+  res.render('catalogo', { title:'Catálogo | Odín', productos });
+});
 
 
 module.exports = router;
